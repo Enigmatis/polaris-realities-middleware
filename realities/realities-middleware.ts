72557221df774de52b9991c99b75d16d5e863820
chenshoo <chenshooker@gmail.com>
2019-09-10 12:52:22 +0300

    realities middleware with linked oper

--- realities/realities-middleware.ts
+++ realities/realities-middleware.ts
@@ -6,25 +6,24 @@ const includeLinkedOperHeaderName: string = "include-linked-oper";
 const realitiesMiddleware: IMiddleware = async (resolve: any, root: any, args: any, context: any, info: any) => {
     const result = await resolve(root, args, context, info);
     const operationalRealityId: number = 0;
-    const realityIdHeaderExists: any = context.realityId === 0 ? true : context.realityId;
-    const isSubEntity: boolean = true;
+    const noRealityIdOrSameAsHeader = entity => entity.realityId == undefined || entity.realityId == context.realityId;
     if (!root) { // assert that its a root resolver
-        // if (!context.realityId ||
-        if (result instanceof Array && realityIdHeaderExists) {
-            return result.filter(entity => entity.realityId === context.realityId ||
-                context.includeLinkedOper === true && entity.realityId === operationalRealityId);
+        if (result instanceof Array) {
+            return result.filter(noRealityIdOrSameAsHeader);
         } else {
-            return result;
+            if (noRealityIdOrSameAsHeader(result))
+                return result;
         }
     } else {
-        return result;
+        if (noRealityIdOrSameAsHeader(result) || (context.includeLinkedOper && result.realityId == operationalRealityId))
+            return result;
     }
 };
 
 const initContextForRealities = async ({req}) => {
     return {
-        realityId: req.headers[realityIdHeaderName],
-        includeLinkedOper: req.headers[includeLinkedOperHeaderName]
+        realityId: req.headers[realityIdHeaderName] ? req.headers[realityIdHeaderName] : 0,
+        includeLinkedOper: req.headers[includeLinkedOperHeaderName] == "false" ? false : req.headers[includeLinkedOperHeaderName]
     };
 };
 
